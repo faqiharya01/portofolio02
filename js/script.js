93117a9bda37efa4bb9de73b93eeb49820cbedaa
faqiharya <faqiharya00@gmail.com>
2023-10-24 00:53:37 +0700

    portofolio

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+//ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// kelik di luar sidebar untuk menghialngkan nav bar
+
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
